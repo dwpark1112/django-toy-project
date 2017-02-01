541b5c94fb56b5da34e60d9e0ff1e50a09064da0
dwpark1112 <dwpark@radnet.kr>
2017-02-01 16:45:01 +0900

    #2 done, css file 만들기

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statics')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 #from bookmark.views import *
+from mysite.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'blog/', include('blog.urls', namespace='blog'))
 
