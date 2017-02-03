af06dbb12a2e50be450ce36c0f2eb04219c63a00
dwpark1112 <dwpark@radnet.kr>
2017-02-03 12:35:35 +0900

    #13 progress, blog & bookmark edit function, edit models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,11 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.urlresolvers import reverse
 from tagging.fields import TagField
+
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -14,6 +17,7 @@ class Post(models.Model):
     create_date = models.DateTimeField('Create Date', auto_now_add=True)
     modify_date = models.DateTimeField('Modify Date', auto_now=True)
     tag = TagField()
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         verbose_name = 'post'
@@ -32,3 +36,8 @@ class Post(models.Model):
 
     def get_next_post(self):
         return self.get_next_by_modify_date()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(Post, self).save(*args, **kwargs)
